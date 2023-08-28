4926fc5b1ea152819e75d326d855d54ce38f68b0
Aaron Warsylewicz <aaron@warsylewicz.ca>
2023-08-28 15:12:34 -0600

    Add Week 2 Content

--- components/Sandbox.js
+++ components/Sandbox.js
@@ -3,14 +3,17 @@ import { Sandpack } from "@codesandbox/sandpack-react";
 import { neoCyan } from "@codesandbox/sandpack-themes";
 
 const Sandbox = ({ template, dependencies = {}, files }) => (
-  <Sandpack
-    theme={neoCyan}
-    template={template}
-    customSetup={{
-      dependencies,
-    }}
-    files={files}
-  />
+  <>
+    <br />
+    <Sandpack
+      theme={neoCyan}
+      template={template}
+      customSetup={{
+        dependencies,
+      }}
+      files={files}
+    />
+  </>
 );
 
 export default Sandbox;
---
+++ pages/week2/activity.js
@@ -0,0 +1,66 @@
+// App.js
+const NavBar = `function NavBar() {
+  let currentPage = "Home";
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>currentPage</li>
+        <li>About</li>
+      <ul>
+    </nav>
+  );
+}
+`;
+
+const NavBarSolution = `export function NavBar() {
+  let currentPage = "Home";
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>{currentPage}</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+`;
+
+const App = `import { NavBar } from "./NavBar";
+
+export default function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <NavBar />
+    </div>
+  );
+}
+`;
+
+// Location of file as key (always starts with /)
+export const files = {
+  "/NavBar.js": {
+    code: NavBar,
+  },
+  "/App.js": {
+    code: App,
+  },
+};
+
+export const filesSolution = {
+  "/NavBar.js": {
+    code: NavBarSolution,
+  },
+  "/App.js": {
+    code: App,
+  },
+};
+
+/* Foo is used to solve the bug: Build optimization failed:
+  found page without a React Component as default export in pages/week1/samples-demo
+*/
+export default function Foo() {
+  return <div>Foo</div>;
+}
