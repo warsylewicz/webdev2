11f52c97f6f7d930bf2accc06ac05525c4c5a4a5
Aaron Warsylewicz <aaron@warsylewicz.ca>
2023-08-31 07:24:12 -0600

    Code refactor

--- components/AssignmentChecker.js
+++ components/AssignmentChecker.js
@@ -3,52 +3,44 @@ import styles from "./AssignmentChecker.module.css";
 
 function AssignmentChecker({ week }) {
   const [account, setAccount] = useState("");
-  const [accountExists, setAccountExists] = useState(null);
-  const [repoExists, setRepoExists] = useState(null);
-  const [weekExists, setWeekExists] = useState(null);
+  const [status, setStatus] = useState({ loading: false, error: null });
 
   const repoName = `cprg306-assignments`;
   // const repoName = `webdev2-demos`;
 
-  const checkUrl = async () => {
+  const checkUrl = async (url) => {
     try {
-      const response = await fetch(
-        `/api/checkUrl?url=https://github.com/${account}`
-      );
+      const response = await fetch(`/api/checkUrl?url=${url}`);
+      if (!response.ok) throw new Error("Network response was not ok");
       const data = await response.json();
-      setAccountExists(data.exists);
+      return data.exists;
     } catch (error) {
       console.error(error);
-      setAccountExists(false);
+      setStatus({ loading: false, error: error.message });
+      return false;
     }
+  };
 
-    try {
-      const response = await fetch(
-        `/api/checkUrl?url=https://github.com/${account}/${repoName}`
-      );
-      const data = await response.json();
-      setRepoExists(data.exists);
-    } catch (error) {
-      console.error(error);
-      setRepoExists(false);
-    }
-
-    try {
-      const response1 = await fetch(
-        `/api/checkUrl?url=https://github.com/${account}/${repoName}/tree/master/app/week${week}`
-      );
-      const data1 = await response1.json();
-
-      const response2 = await fetch(
-        `/api/checkUrl?url=https://github.com/${account}/${repoName}/tree/main/app/week${week}`
-      );
-      const data2 = await response2.json();
-
-      setWeekExists(data1.exists || data2.exists);
-    } catch (error) {
-      console.error(error);
-      setWeekExists(false);
-    }
+  const checkAccount = async () => {
+    setStatus({ loading: true, error: null });
+    const accountExists = await checkUrl(`https://github.com/${account}`);
+    const repoExists =
+      accountExists &&
+      (await checkUrl(`https://github.com/${account}/${repoName}`));
+    const weekExistsInMaster = await checkUrl(
+      `https://github.com/${account}/${repoName}/tree/master/app/week${week}/page.js`
+    );
+    const weekExistsInMain = await checkUrl(
+      `https://github.com/${account}/${repoName}/tree/main/app/week${week}/page.js`
+    );
+    const weekExists = weekExistsInMain || weekExistsInMaster;
+    setStatus({
+      loading: false,
+      error: null,
+      accountExists,
+      repoExists,
+      weekExists,
+    });
   };
 
   return (
@@ -63,19 +55,29 @@ function AssignmentChecker({ week }) {
         placeholder="GitHub username"
         onChange={(event) => setAccount(event.target.value)}
       />
-      <button onClick={checkUrl} className={styles.button}>
+      <button
+        onClick={checkAccount}
+        className={styles.button}
+        disabled={status.loading}
+      >
         Check Assignment
       </button>
-      {accountExists === false && <p>Account does not exist!</p>}
-      {accountExists === true && repoExists === false && (
+      {status.loading && <p>Please wait...</p>}
+      {status.error && <p>Error: {status.error}</p>}
+      {status.accountExists === false && <p>Account does not exist!</p>}
+      {status.accountExists === true && status.repoExists === false && (
         <p>Repository does not exist!</p>
       )}
-      {accountExists === true &&
-        repoExists === true &&
-        weekExists === false && <p>Week {week} does not exist in your repo!</p>}
-      {accountExists === true && repoExists === true && weekExists === true && (
-        <p>Congratulations! Week {week} exists in your repo!</p>
-      )}
+      {status.accountExists === true &&
+        status.repoExists === true &&
+        status.weekExists === false && (
+          <p>Week {week} does not exist in your repo!</p>
+        )}
+      {status.accountExists === true &&
+        status.repoExists === true &&
+        status.weekExists === true && (
+          <p>Congratulations! Week {week} exists in your repo!</p>
+        )}
     </div>
   );
 }
